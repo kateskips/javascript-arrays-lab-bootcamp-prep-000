f1366b41881478cbfbf3183325a1cc27fe5c848d
kateskips <follett.katie@gmail.com>
2018-08-01 00:31:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
 }
+
+function appendKitten(name){
+  var newKittens = [...kittens];
+  newKittens.push(name);
+  return newKittens;
+}
